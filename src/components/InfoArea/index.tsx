272b92cf25f326008630d058187351f72e6ae5c3
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2022-03-15 16:45:04 -0300

    contruindo a area

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -2,7 +2,13 @@ import * as C from './styles'
 export const InfoArea = ()=> {
     return(
         <C.Container>
-            ....
+            <C.MontArea>
+                <C.MonthArrow></C.MonthArrow>
+
+            </C.MontArea>
+            <ResumeArea>
+
+            </ResumeArea>
         </C.Container>
 
     );
